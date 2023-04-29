7b3410699e573f8d804289dfb7b0ca85e4bc7bf2
sunjin <sjink@pm.me>
2023-04-29 13:07:32 -0400

    created page routes

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+const Home = () => {
+  return (
+    <div>
+      <NavBar/>
+      Home
+    </div>
+  )
+}
+
+export default Home
