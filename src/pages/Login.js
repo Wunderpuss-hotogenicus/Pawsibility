c817b6ea2235400a19a1261582febf12340ebab2
frahman <faisalrahman3141@gmail.com>
2023-04-29 16:44:56 -0400

    worked on login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    // Handle form submission logic here
+    axios.post('/api/login', {
+      username,
+      password
+    })
+    .then(response => {
+      //redirect to home
+      console.log(data);
+
+      })
+    .catch(err => 
+        console.log(`Username: ${username}, Password: ${password}`);
+        setUsername('');
+        setPassword('')
+    })   
+ }
+  
+  
   return (
-    <div>
-      Login What up
-    </div>
+   <div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+   </div>
   )
 }
 
