d5efd32ecf0f4ccb1fb201a637ee3a81cae713b2
Andrew <androoo.1997@gmail.com>
2023-04-29 11:56:08 -0400

    made changes to webpack and started on server files

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-const path = require('path');
+// const path = require('path');
+
+fetch('https://api.petfinder.com/v2/animals?type=dog', {
+  headers: {
+    Authorization: 'Bearer BnOiXaaLRv1CbvQeiwUrjKtQgIH2DRpTUEYXAfcSgSzFhvg1JX'
+  }
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from “react”;
-import  {render} from ‘react-dom’;
-import App from ‘./components/App’;
-render(<App />, document.getElementById(‘root’));
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+render(<App />, document.getElementById('root'))
